a003c2eb20ad541e19c1fe605256e35d900bb5ae
ORenee <Oldsyellow@gmail.com>
2018-10-24 18:57:11 -0500

    Started lesson 2 and finnished

---
+++ src/com/team2052/KnightKrawlerIntroJava/Lesson02/AreaCalculator.java
@@ -0,0 +1,23 @@
+package com.team2052.KnightKrawlerIntroJava.Lesson02;
+
+public class AreaCalculator {
+
+    public static void main(String[] args) {
+        AreaCalculator areaThing = new AreaCalculator();
+        int rectArea = areaThing.rectangleArea( 4, 5);
+        System.out.println( rectArea );
+        double cirArea = areaThing.circleArea( 3 );
+        System.out.println( cirArea );
+        
+    }
+
+
+    private double circleArea(int radius) {
+        return radius * radius * Math.PI;
+    }
+
+    private int rectangleArea( int hight , int width) {
+        return hight * width;
+    }
+
+}
