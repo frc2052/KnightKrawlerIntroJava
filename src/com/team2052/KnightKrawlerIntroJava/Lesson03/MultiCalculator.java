88fa8d5ec1d7328d2c757f9e60e361889db304c5
Blazin_Samich <sebas13sar@gmail.com>
2018-11-26 19:52:34 -0600

    worked on lesson 3 and put some finishing touches to lesson 2

---
+++ src/com/team2052/KnightKrawlerIntroJava/Lesson03/MultiCalculator.java
@@ -0,0 +1,39 @@
+package com.team2052.KnightKrawlerIntroJava.Lesson03;
+
+public class MultiCalculator {
+
+
+    private int number1;
+    public int getNumber1() {
+        return number1;
+    }
+    public void setNumber1(int val){
+        if (val < 0) {
+            number1 = 0;
+        } else {
+            number1 = val;
+        }
+    }
+
+    private int number2;
+    public int getNumber2() {
+        return number1;
+    }
+    public void setNumber2(int val){
+        if (val < 0) {
+            number1 = 0;
+        } else {
+            number1 = val;
+        }
+    }
+
+    public int calculate() {
+        return getNumber1() + getNumber2();
+        int sum = number1 + number2;
+        System.out.println("this is the sum"+ sum );
+    }
+}
+
+
+
+
