05c9b8c5c651e63f71672cbe744d674cc990fdba
LexiB <2024birka54@moundsviewschools.org>
2021-08-16 20:04:20 -0500

    working on Lesson 3, assignment 6. Currently not working/stuck.

--- src/com/team2052/javaintro/lesson03/Lesson3Runner.java
+++ src/com/team2052/javaintro/lesson03/Lesson3Runner.java
@@ -13,16 +13,25 @@ public class Lesson3Runner {
 
     public void run()
     {
-        System.out.println("What would you like to do?");
-        System.out.println("1. add two numbers");
-        int option = scanner.nextInt();
-        if (option == 1) {
-            input();
-        } else if (option == 2) {
-            //subtract();
-        } else {
+            MultiCalculator calc = new MultiCalculator();
+            System.out.println("Please enter method of calculation.");
+            System.out.println("1--> ADD");
+            System.out.println("2--> SUBTRACT");
+            System.out.println("3--> MULTIPLY");
+            System.out.println("4--> DIVIDE");
+             int option = scanner.nextInt();
+            if (option == 1) {
+                calc.setCalcOperation(MultiCalculator.Operation.ADD);
+            } else if (option == 2) {
+                calc.setCalcOperation(MultiCalculator.Operation.SUBTRACT);
+            } else if (option == 3) {
+                calc.setCalcOperation(MultiCalculator.Operation.MULTIPLY);
+            } else if (option == 4) {
+                calc.setCalcOperation(MultiCalculator.Operation.DIVIDE);
+            }else {
             System.out.println("Invalid option. Returning to main menu.");
-        }
+            }
+            input();
     }
 
     private void input() {
@@ -35,12 +44,7 @@ public class Lesson3Runner {
             System.out.println("Please enter the second number");
             int number2 = scanner.nextInt();
             calc.setNumber2(number2);
-            int sum = calc.calculate();
-            System.out.println("The sum of those two numbers is " + sum);
-            System.out.println("Run Again? (enter \"y\" to continue)");
-            scanner.nextLine(); //scanner isn't very smart. this will pickup the last return after "nextInt", clear it
-            String prompt = scanner.nextLine();
-            keepGoing = prompt.trim().toLowerCase().equals("y");
+            System.out.println("Answer " + calc.calculate());
         }
     }
 }
--- src/com/team2052/javaintro/lesson03/MultiCalculator.java
+++ src/com/team2052/javaintro/lesson03/MultiCalculator.java
@@ -2,38 +2,59 @@ package com.team2052.javaintro.lesson03;
 
 
 public class MultiCalculator {
-    
-    
+    private int number1;
+    private int number2;
+    private Operation calcOperation;
+
     public MultiCalculator() {
         
     }
 
 
-    private int number1;
     public int getNumber1() {
         return number1;
     }
+
     public void setNumber1(int val){
-        if (val < 0) {
-            number1 = 0;
-        } else {
-            number1 = val;
-        }
+        number1 = val;
     }
 
-    private int number2;
     public int getNumber2() {
         return number2;
     }
-    public void setNumber2(int val2){
-        if (val2 < 0) {
-            number2 = 0;
+
+    public void setNumber2(int val){
+        number2 = val;
+    }
+
+        
+    
+    public Operation getCalcOperation() {
+        return calcOperation;
+    }
+    public void setCalcOperation(Operation val){
+        calcOperation = val;
+    }
+
+    public int calculate() {
+        if (getCalcOperation() == Operation.ADD) {
+            return this.getNumber1() + this.getNumber2();
+        } else if (getCalcOperation() == Operation.SUBTRACT) {
+            return this.getNumber1() - this.getNumber2();
+        } else if (getCalcOperation() == Operation.MULTIPLY) {
+            return this.getNumber1() * this.getNumber2();
+        } else if (getCalcOperation() == Operation.DIVIDE) {
+            return this.getNumber1() / this.getNumber2();
         } else {
-            number2 = val2;
+            System.out.println("Error :(");;
         }
     }
 
-        public int calculate() {
-            return getNumber1() + getNumber2();
-        }    
+    
+    public enum Operation {
+        ADD,
+        SUBTRACT,
+        MULTIPLY,
+        DIVIDE
+    }
 }
