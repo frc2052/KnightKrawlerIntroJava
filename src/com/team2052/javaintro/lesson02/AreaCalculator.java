a00e9c7e3d13f3f9cc9a20142fd8fcb45b1e570e
JJ <2023fleij24@moundsviewschools.org>
2021-11-01 19:53:31 -0500

    lesson two half complete

---
+++ src/com/team2052/javaintro/lesson02/AreaCalculator.java
@@ -0,0 +1,17 @@
+package com.team2052.javaintro.lesson02;
+import java.util.Scanner
+public class AreaCalculator {
+    private double circleArea(int radius) {
+        return radius * radius * Math.PI;
+   
+    }
+    private int rectangleArea(int height, int width){
+
+
+        return height + width;
+    }
+    private double run(){
+        System.out.println("enter radius");
+       int radius = scanner.nextInt();
+        
+    }
